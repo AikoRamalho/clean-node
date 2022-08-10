8ca8b4a15efcffdb33988dab915c660d3df9372d
Aiko Ramalho <aiko.ramalho@voaed.com>
2022-08-10 19:16:14 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  roots: ['<rootDir>/src'],
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}

20ebf7a6f01b145dbab7ed2cfe94daf3d956e7dd
Aiko Ramalho <aiko.ramalho@voaed.com>
2022-08-11 13:20:30 -0300

    refactor: simplify SignUpController required fields logic

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,13 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    // TODO: create validation class
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
     throw new Error('Method not implemented.')
   }

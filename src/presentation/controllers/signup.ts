3d8f971798f3848d784f4bee5157424a1d690d4f
Aiko Ramalho <aiko.ramalho@voaed.com>
2022-08-12 12:44:29 -0300

    feat: ensure SignUpController implements emailValidator

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,18 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  // criar um validador atributo/metodo da classe
+  // faz um validor.create()
   handle (httpRequest: HttpRequest): HttpResponse {
     // TODO: create validation class
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
@@ -13,6 +23,9 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) return badRequest(new InvalidParamError('email'))
     throw new Error('Method not implemented.')
   }
 }

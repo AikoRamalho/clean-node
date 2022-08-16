5f4ebb0d9cc25a0fe70bdd5c2e6fba651ca13aa9
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-08-16 02:03:31 -0300

    refactor: move success http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
 })
+
+export const ok = <GenericData>(data: GenericData): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
